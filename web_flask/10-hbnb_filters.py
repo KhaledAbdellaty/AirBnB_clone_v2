0d3c0308d62ceae251a5d63d3259ae85e192fea5
KhaledAbdellaty <kabdellaty@gmail.com>
2024-04-20 23:34:27 -0700

    11. HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A Flask web application.
+
+runs on host=0.0.0.0, port=5000"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filter():
+    """This route that displays HBNB filters HTML page with dynamic data"""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def tearDown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
