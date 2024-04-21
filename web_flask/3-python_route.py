79252083025688550cb88ac380ddd8debeefb6a6
KhaledAbdellaty <kabdellaty@gmail.com>
2024-04-20 21:38:15 -0700

    3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A Flask web application.
+
+runs on host=0.0.0.0, port=5000"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """The initial route that displays, 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbhb_route():
+    """The '/hbnb route that displays, 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """The '/c/' route that take variable and display it"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is_cool'):
+    """The '/python/' route have default value and accepts variable"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
