4e2627c0a2e7f8a845b93e9fbafd748376b18a29
KhaledAbdellaty <kabdellaty@gmail.com>
2024-04-25 04:58:21 -0700

    fix 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -4,6 +4,8 @@
 runs on host=0.0.0.0, port=5000"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
@@ -11,8 +13,8 @@ app = Flask(__name__)
 @app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filter():
     """This route that displays HBNB filters HTML page with dynamic data"""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
     return render_template("10-hbnb_filters.html",
                            states=states, amenities=amenities)
 
